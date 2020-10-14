8c0012ffb27a2019c9288d64d16a45f5d9e808e0
Teurowel <teurowel@gmail.com>
2020-10-14 21:11:35 +0900

    Zombie move

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,32 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] protected int hp = 100; //Health for enemy
+    [SerializeField] protected int maxHp = 100; //Max health for enemy
+    [SerializeField] protected int damage = 10; //damage for enemy
+    [SerializeField] protected int moveSpeed = 3; //move speed for enemy
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Debug.Log("Enemy update");
+        //Basic movement
+        Move();
+    }
+
+    protected virtual void Move()
+    {
+        transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0f, 0f);
+    }
+}
