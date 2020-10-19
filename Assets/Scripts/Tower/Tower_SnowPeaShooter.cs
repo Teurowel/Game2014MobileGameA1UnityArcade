c72f094a207e9ff7a8bffe63004a03400f522a67
Teurowel <teurowel@gmail.com>
2020-10-19 14:50:45 +0900

    Added SnowPeaShooter

---
+++ Assets/Scripts/Tower/Tower_SnowPeaShooter.cs
@@ -0,0 +1,27 @@
+﻿//SourceFileName : Tower_SnowPeaShooter.cs
+//Author's name : Doosung Jang
+//Studnet Number : 101175013
+//Date last Modified : Oct.19, 2020
+//Program description : SnowPeaShooter class
+//Revision History : Oct.19, 2020 : Created
+
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_SnowPeaShooter : ActiveTower
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        // Debug.Log("Tower_PeaShooter Start");
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
