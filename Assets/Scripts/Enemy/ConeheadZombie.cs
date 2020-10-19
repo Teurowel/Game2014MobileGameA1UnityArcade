6db7398903a9e0e33ebbb56f159b77726a65fdb4
Teurowel <teurowel@gmail.com>
2020-10-19 22:50:07 +0900

    Added ConeheadZombie

---
+++ Assets/Scripts/Enemy/ConeheadZombie.cs
@@ -0,0 +1,33 @@
+﻿//SourceFileName : ConeheadZombie.cs
+//Author's name : Doosung Jang
+//Studnet Number : 101175013
+//Date last Modified : Oct.19, 2020
+//Program description : Conehead Zombie is stronger than normal zombie
+//Revision History : Sep.18, 2020 Created
+//                   Oct.18, 2020 Added play sound
+
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeheadZombie : Enemy
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        //Play sound
+        if (SoundManager.instance != null)
+        {
+            SoundManager.instance.Play("ZombieSFX");
+        }
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        //Debug.Log("Zombie update");
+        base.Update();
+    }
+}
